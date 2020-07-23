70bf01fab98b3e4745e1539e90be98829e022ccd
sunshan <735160133@qq.com>
2020-07-23 17:00:04 +0800

    进出库日志药品名称模糊搜索

--- pages/cangku/stockInOut/stockInOut.js
+++ pages/cangku/stockInOut/stockInOut.js
@@ -2,7 +2,10 @@ var http = require("../../../utils/httpUtil.js")
 Page({
   data: {
     listData:[],
-    screenHeight:''
+    screenHeight:'',
+    searchValue:"",
+    company_id:0,
+
   },
   onLoad: function (options) {
     var role_id = wx.getStorageSync('role_id');
@@ -12,6 +15,8 @@ Page({
     }else if(role_id ==3){
       company_id= Number(options.selectId)
     }
+    this.setData({company_id:company_id})
+
     var that = this //很重要，一定要写
     var params={ "company_id":company_id}
     http.Post('/app/storage/log/company/query', params, function (res) {
@@ -32,5 +37,46 @@ Page({
       },
     })
   },
-  
+  onSearch(){
+    console.log("search value:",this.data.searchValue)
+    var that = this
+    var params={
+      "user_id":this.data.userId,
+    "research_name":this.data.searchValue
+    }
+    http.Post('/app/dosage/query/like', params,function (res) {
+      var datas=res.data;//res.data就是从后台接收到的值
+       that.setData({//循环完后，再对list进行赋值
+         List: datas.data,
+         loading: false
+       })
+     })
+  },
+  onChange(e) {
+   
+    this.setData({
+      searchValue: e.detail,
+    });
+    this.storageLog();
+    console.log("search value:",e.detail)
+  },
+  onCancel(){
+    this.setData({
+      searchValue: "",
+    });
+  },
+  storageLog(){
+    var that = this //很重要，一定要写
+    //var valueStrArr=that.data.searchValue.split("")
+    var params={ "company_id": that.data.company_id,"medicine_name":valueStrArr}
+    http.Post('/app/storage/log/filter/query', params, function (res) {
+        var resData =res.data.data
+        console.log(resData)
+        that.setData({//循环完后，再对list进行赋值
+          listData: res.data.data,
+          loading: false
+        })
+
+    })
+  },
 })
